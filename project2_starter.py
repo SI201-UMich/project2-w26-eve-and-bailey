aba112058149a0374dda7f69d97ead08afe77044
Bailey Ellul <bellul@umich.edu>
2026-03-31 10:04:26 -0400

    ec

--- project2_starter.py
+++ project2_starter.py
@@ -305,7 +305,7 @@ def google_scholar_searcher(query):
     # ==============================
     # YOUR CODE STARTS HERE
     # ==============================
-    pass
+    
     # ==============================
     # YOUR CODE ENDS HERE
     # ==============================
